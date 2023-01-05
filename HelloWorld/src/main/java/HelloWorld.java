39b9821e6ac6665395faa296dff61182bc99c277
Seungkyun1976 <seungkyun.lee@outlook.com>
2023-01-05 17:57:15 +0000

    Saved progress on HelloWorld 2023-01-05 17:57:15 +0000

--- HelloWorld/src/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 
 import org.junit.Assert;
-import org,junit.test
+import org.junit.test
 
 public class HelloWorld {
     HelloWorld hw = new HelloWorld();
@@ -10,14 +10,9 @@ public class HelloWorld {
         string actual = hw.sayhello().trim();
         Assert.assertEquals(expected,actual);
     }
-}
     /**
      * This method must return a hello world String, instead of returning "change me".
      * A test case will check if the behavior is as expected.
      * You can use the test results to check for differences.
      * @return "Hello, world!"
      */
-    public String sayHello(){
-        return "change me";
-    }
-}
