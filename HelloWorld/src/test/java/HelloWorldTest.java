6d8537ce8b40e234bc72193e5274eebd04b97ce0
Seungkyun1976 <seungkyun.lee@outlook.com>
2023-01-06 17:54:21 +0000

    Saved progress on HelloWorld 2023-01-06 17:54:21 +0000

--- HelloWorld/src/test/java/HelloWorldTest.java
+++ HelloWorld/src/test/java/HelloWorldTest.java
@@ -1,17 +1,24 @@
 
-import org.junit.Assert;
-import org.junit.Test;
 
 public class HelloWorldTest {
     HelloWorld hw = new HelloWorld();
-
+    
     /**
      * Method sayHello() must return "Hello, World!". The trim method will remove any extra spaces or newlines at the end.
      */
     @Test
     public void helloTest(){
         String expected = "Hello, world!";
-        String actual = hw.sayHello().trim();
+        String actual = hw.HelloWorld().trim();
         Assert.assertEquals(expected,actual);
+        System.out.println("Hello World!");
+
     }
+
+    public void setHw(HelloWorld hw) {
+        this.hw = hw;
+    }
+   
+
 }
+return = HelloWorld;
